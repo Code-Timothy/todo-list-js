1e73e66a9487b8cf71e37978166034ec8476e6c1
Tymoteusz Wójcik <timmywojcik@gmail.com>
2023-08-09 22:05:46 +0200

    addNewTask funciton added

--- js/script.js
+++ js/script.js
@@ -10,6 +10,13 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -22,8 +29,24 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        } else {
+            addNewTask(newTaskContent);
+        }
+
+    };
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
