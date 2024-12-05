e3b987b63c1cf5e02f299ca87b7d8db54db6fcec
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-05 20:30:30 +0100

    binding events moved to a separate function

--- js/script.js
+++ js/script.js
@@ -45,6 +45,24 @@
         });
     };
 
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    };
+
     const renderTasks = () => {
         let htmlString = "";
 
@@ -60,21 +78,7 @@
 
         document.querySelector(".js-tasksList").innerHTML = htmlString;
 
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(index);
-            })
-        });
-
-        const toggleDoneButtons = document.querySelectorAll(".js-done");
-
-        toggleDoneButtons.forEach((toggleDoneButton, index) => {
-            toggleDoneButton.addEventListener("click", () => {
-                toggleTaskDone(index);
-            });
-        });
+        bindEvents();
     };
 
     const init = () => {
