4b486cc8099177eb2faec4dc315b62dd08459a18
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-07 18:48:38 +0100

    remove task function is now compliant with immutability

--- js/script.js
+++ js/script.js
@@ -11,7 +11,11 @@
     };
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
+        ];
+
         renderTasks();
     };
 
